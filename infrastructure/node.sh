7b965673d91fb321cc04a88a01c5754380a1c297
Ben Lackey <ben.lackey@outlook.com>
2018-09-24 14:33:10 -0700

    java

--- infrastructure/node.sh
+++ infrastructure/node.sh
@@ -2,9 +2,13 @@
 
 echo "Running node.sh"
 
-# JDK 8u152
+# Java
+echo "Installing Oracle Java 8 JDK"
+cd ~
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+yum localinstall jdk-8u152-linux-x64.rpm
 
-# DSE 4.8.15
+# DataStax
 echo "
 [datastax]
 name = DataStax Repo for DataStax Enterprise
@@ -12,5 +16,4 @@ baseurl=https://datastax@oracle.com:*9En9HH4j^p4@rpm.datastax.com/enterprise
 enabled=1
 gpgcheck=0
 " > /etc/yum.repos.d/datastax.repo
-
 yum install dse-full-4.8.15-1
