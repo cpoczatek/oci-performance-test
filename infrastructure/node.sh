938fe745b92ed95042bbe0f50d03ce5637dbc7ca
Ben Lackey <ben.lackey@outlook.com>
2018-09-25 18:35:18 -0700

    comments

--- infrastructure/node.sh
+++ infrastructure/node.sh
@@ -5,6 +5,7 @@ echo "Running node.sh"
 #######################################################
 ######################### Disks #######################
 #######################################################
+echo "Formatting the drives..."
 
 ## Give ISCSI time to intiate
 iscsi="1"
@@ -67,13 +68,15 @@ fi
 #######################################################
 ######################### Java ########################
 #######################################################
-echo "Installing Oracle Java 8 JDK"
+echo "Installing Oracle Java 8 JDK..."
 wget -O ~/jdk8.rpm -N --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm
 yum -y localinstall ~/jdk8.rpm
 
 #######################################################
 ####################### DataStax ######################
 #######################################################
+echo "Installing DataStax..."
+
 echo "[datastax]
 name = DataStax Repository
 baseurl=https://datastax%40oracle.com:*9En9HH4j^p4@rpm.datastax.com/enterprise
@@ -81,7 +84,7 @@ enabled=1
 gpgcheck=0" > /etc/yum.repos.d/datastax.repo
 yum -y install dse-full-4.8.15-1
 
-# Need to configure the data and commitlog directories
+# Configure the data and commitlog directories
 mkdir -p /data0/cassandra/data
 chown -R cassandra /data0/cassandra/data
 chgrp -R cassandra /data0/cassandra/data
