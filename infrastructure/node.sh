435e57d4f008b375d08c7f0ec41b9fca07075ce5
Ben Lackey <ben.lackey@outlook.com>
2018-09-25 20:02:41 -0700

    newline

--- infrastructure/node.sh
+++ infrastructure/node.sh
@@ -9,7 +9,6 @@ echo "Installing Oracle Java 8 JDK..."
 wget -O ~/jdk8.rpm -N --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm
 yum -y localinstall ~/jdk8.rpm
 
-
 #######################################################
 ######################### Disks #######################
 #######################################################
